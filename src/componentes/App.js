930f7190df81857373fdf197268de7c69dfe099b
brendiTech <brenditech@gmail.com>
2020-07-09 02:22:05 -0300

    Crear formulario

---
+++ src/componentes/App.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import '../css/App.css';
+import Header from './Header';
+import Formulario from './Formulario';
+import Listado from './Listado';
+
+class App extends Component {
+
+    state = {
+      presupuesto: '',
+      restante: '',
+      gastos: {}
+    }
+
+
+  //Agregar un nuevo gasto al state
+  agregarGasto = gasto => {
+    //Tomar una copia del state actual
+    const gastos = {...this.state.gastos};
+
+    console.log('se agregó el gasto' + gasto)
+    console.log(gastos)
+    //Agregar al gasto al objeto del state
+    gastos[`gasto${Date.now()}`]=gasto
+    //Ponerlo en state
+    this.setState({
+      gastos
+    })
+  }
+
+  render(){
+    return (
+      <div className="App container">
+        <Header
+          titulo='Gasto semanal'
+        />
+        <div className="contenido-principal contenido">
+          <div className="row">
+            <div className="one-half column">
+              <Formulario
+              agregarGasto = {this.agregarGasto}
+              />
+            </div>
+            <div className="one-half column">
+              <Listado
+              gastos={this.state.gastos}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/componentes/Gasto.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+class Gasto extends Component {
+
+    render() {
+        return (
+            <div className="class-name">
+                Desde gasto.js
+            </div>
+        );
+    }
+}
+
+export default Gasto;
